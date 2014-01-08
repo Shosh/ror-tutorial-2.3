60924ba53eed9d16d1801c56a35b393de3936f41
Margarita Vasileva <m.h.vasileva@gmail.com`>
2014-01-08 18:54:25 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,16 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have the base title 'Home'" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                :text => "| Home")
     end
   end
 
@@ -23,10 +29,16 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have the base title 'Help'" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title',
+                                    :text => "| Help")
     end
   end
 
@@ -37,10 +49,16 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have the base title 'About Us'" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title',
+                                    :text => "| About Us")
     end
   end
 end
